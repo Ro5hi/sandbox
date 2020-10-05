1c204c9de8e5d197c30ddc7bb4231f6b91b2b1b2
Shirlen Detablan <slen.detablan@gmail.com>
2020-10-05 14:11:33 -0400

    frontend WIP, render & script.js

--- back/app/controllers/subscriptions_controller.rb
+++ back/app/controllers/subscriptions_controller.rb
@@ -1,2 +1,17 @@
 class SubscriptionsController
+
+    def index
+        @subscriptions = Subscription.all 
+        render json: @subscriptions, include: :user
+    end 
+
+    def create
+    end 
+
+    def update
+    end 
+
+    def delete
+    end 
+
 end 
---
+++ front/src/script.js
@@ -0,0 +1,25 @@
+console.log("SubTrack: Track your Subscriptions")
+
+
+let SubTrack = (() => {
+  total = 0, nourishment = 0, service = 0, membership = 0;
+})()
+
+// Create Sub
+
+addSub() {
+  const form = document.getElementById
+}
+
+// Categorize Sub
+
+addCategory() {
+  const label = document.getElementById
+}
+
+// Update Sub
+
+
+// Delete Sub
+
+// Get Input Data
